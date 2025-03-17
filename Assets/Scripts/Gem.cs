68280f75ea4201a0afdfd26000370e2a70c9f07b
Inbisat Naveed <inkyn2002@gmail.com>
2025-03-17 11:52:03 +0100

    Commit jugador pueda ver sus puntos

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,10 +1,15 @@
+using System;
 using UnityEngine;
 
 public class Gem : MonoBehaviour, IItem
 {
+    public static event Action<int> OnGemCollect;
+    public int worth = 0;
+
    
     public void Collect()
     {
+        OnGemCollect.Invoke(worth);
         Destroy(gameObject);
     }
 
